74858ce41f70d0712ec1f4330305576829e8c010
eight_y_88 <jadec.eight@gmail.com>
2022-01-05 15:57:11 +0900

    Add UpdateInfo

---
+++ Client/src/main/java/si/f5/pa_union/update/UpdateList.java
@@ -0,0 +1,52 @@
+package si.f5.pa_union.update;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.components.ObjectSelectionList;
+import net.minecraft.network.chat.Component;
+
+import java.util.List;
+
+public class UpdateList extends ObjectSelectionList<UpdateList.Entry> {
+    int size;
+    public UpdateList(Minecraft p_94010_, int p_94011_, int p_94012_, int p_94013_, int p_94014_, int p_94015_, List<Component> entries) {
+        super(p_94010_, p_94011_, p_94012_, 0, p_94012_ - 32, 2 * p_94010_.font.lineHeight + 8);
+        addEntry(new Entry(entries));
+        size = entries.size();
+    }
+
+    @Override
+    public int getMaxScroll() {
+        return Math.max(0, Minecraft.getInstance().font.lineHeight * size - (this.y1 - this.y0 - 4));
+    }
+
+    @Override
+    public int getScrollBottom() {
+        return super.getScrollBottom();
+    }
+
+    public static class Entry extends ObjectSelectionList.Entry<UpdateList.Entry>  {
+        final List<Component> entry;
+
+        public Entry(List<Component> entry) {
+            this.entry = entry;
+        }
+
+        @Override
+        public void render(PoseStack pStack, int entryIdx, int top, int left, final int entryWidth, final int entryHeight, final int mouseX, final int mouseY, final boolean p_194999_5_, final float partialTicks) {
+            Font font = Minecraft.getInstance().font;
+            final List<Component> strings = entry;
+            int y = top + 2;
+            for (int i = 0; i < Math.max(strings.size(), 2); i++) {
+                font.draw(pStack, strings.get(i), left + 5, y, 0xFFFFFF);
+                y += font.lineHeight;
+            }
+        }
+
+        @Override
+        public Component getNarration() {
+            return entry.get(0);
+        }
+    }
+}
--- Client/src/main/java/si/f5/pa_union/update/UpdateScreen.java
+++ Client/src/main/java/si/f5/pa_union/update/UpdateScreen.java
@@ -1,48 +1,69 @@
 package si.f5.pa_union.update;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.OptionsList;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.network.chat.CommonComponents;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 public class UpdateScreen extends Screen {
     boolean isLoad;
-    static boolean FLAG = true;
+    UpdateList list;
+
     public UpdateScreen() { this(false); }
 
     public UpdateScreen(boolean isLoading) {
         super(new TextComponent("アップデート内容"));
         isLoad = isLoading;
-        if (FLAG) {
-            init();
-            FLAG = false;
-        }
     }
 
     public void init() {
+        super.init();
+        this.clearWidgets();
         try {
-            Path path = Path.of(Objects.requireNonNull(this.getClass().getClassLoader().getResource("update-info.txt")).getPath());
-            List<UpdateEntry> entries = new ArrayList<>();
-            AtomicReference<UpdateEntry> entry = new AtomicReference<>();
+            Path path = Paths.get(Objects.requireNonNull(this.getClass().getClassLoader().getResource("update-info.txt")).toURI());
+            final List<Component> entries = new ArrayList<>();
             Files.readAllLines(path, StandardCharsets.UTF_8).forEach((s -> {
-                if (s.startsWith("#") || entry.get() == null) {
-                    if (entry.get() != null) {
-                        entries.add(entry.get());
-                    }
-                    entry.set(new UpdateEntry(s.replaceFirst("#", "")));
+                if (s.startsWith("#")) {
+                    entries.add(new TextComponent(s.replace("#", "")).withStyle(ChatFormatting.BOLD).withStyle(ChatFormatting.UNDERLINE));
+                } else if (s.startsWith("-")){
+                    entries.add(new TextComponent(s.replace("-", "・")).withStyle(ChatFormatting.BOLD));
                 } else {
-                    entry.get().addLine(s);
+                    entries.add(new TextComponent(s));
                 }
             }));
-        } catch (IOException e) {
+            this.list = new UpdateList(this.minecraft, this.width, this.height, 0, this.height, 25, entries);
+            this.addWidget(list);
+            this.addRenderableWidget(new Button(this.width / 2 - 100, this.height - 27, 200, 20, new TextComponent("閉じる"), (p_96827_) -> {
+                Minecraft.getInstance().setScreen(null);
+            }));
+        } catch (IOException | URISyntaxException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void render(PoseStack p_96562_, int p_96563_, int p_96564_, float p_96565_) {
+        this.renderBackground(p_96562_);
+        this.list.render(p_96562_, p_96563_, p_96564_, p_96565_);
+        super.render(p_96562_, p_96563_, p_96564_, p_96565_);
     }
 }
