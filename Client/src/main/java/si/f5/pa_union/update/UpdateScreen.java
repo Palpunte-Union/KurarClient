2126393fca300bc7529aff114ad54c68441a82d6
eight_y_88 <jadec.eight@gmail.com>
2022-01-05 10:00:55 +0900

    Add UpdateScreen Initialize

--- Client/src/main/java/si/f5/pa_union/update/UpdateScreen.java
+++ Client/src/main/java/si/f5/pa_union/update/UpdateScreen.java
@@ -3,6 +3,15 @@ package si.f5.pa_union.update;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.TextComponent;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
+
 public class UpdateScreen extends Screen {
     boolean isLoad;
     static boolean FLAG = true;
@@ -18,6 +27,22 @@ public class UpdateScreen extends Screen {
     }
 
     public void init() {
-
+        try {
+            Path path = Path.of(Objects.requireNonNull(this.getClass().getClassLoader().getResource("update-info.txt")).getPath());
+            List<UpdateEntry> entries = new ArrayList<>();
+            AtomicReference<UpdateEntry> entry = new AtomicReference<>();
+            Files.readAllLines(path, StandardCharsets.UTF_8).forEach((s -> {
+                if (s.startsWith("#") || entry.get() == null) {
+                    if (entry.get() != null) {
+                        entries.add(entry.get());
+                    }
+                    entry.set(new UpdateEntry(s.replaceFirst("#", "")));
+                } else {
+                    entry.get().addLine(s);
+                }
+            }));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
